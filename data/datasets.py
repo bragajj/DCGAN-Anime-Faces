33a696fc9a3aecf5c962698c3d183e4b58d63acf
ErrorInever <errorinever@gmail.com>
2021-04-12 23:37:48 +0000

    add anime dataset

---
+++ data/datasets.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class AnimeFacesDataset(Dataset):
+    def __init__(self, img_folder):
+        self.img_folder = img_folder
+        self.img_names = [n for n in os.listdir(img_folder) if n.endswith(('png', 'jpeg', 'jpg'))]
+
+    def __getitem__(self, idx):
+        img = Image.open(os.path.join(self.img_folder, self.img_names[idx])).convert('RGB')
+        return self.transform(img)
+
+    def __len__(self):
+        return len(self.img_names)
+
+    @property
+    def transform(self):
+        return transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
