42fd9a55fe4aedfd419f72333f86aba84ba867b6
ErrorInever <errorinever@gmail.com>
2021-04-12 23:23:23 +0000

    init config

---
+++ config.py
@@ -0,0 +1,20 @@
+from easydict import EasyDict as edict
+
+__C = edict()
+
+cfg = __C
+# Global
+__C.NUM_EPOCHS = 1
+__C.LEARNING_RATE = 1e-4
+__C.BATCH_SIZE = 128
+# IMG
+__C.IMG_SIZE = 64
+__C.CHANNELS_IMG = 3
+__C.Z_DIMENSION = 100
+# Models
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+# Paths and saves
+__C.SAVE_EACH_EPOCH = 10
+__C.OUT_DIR = ""
+__C.SAVE_MODELS_PATH = ""
