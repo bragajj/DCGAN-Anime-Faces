9cddf3101164369e3bacedd4b3b36d2d76bdbb5a
ErrorInever <errorinever@gmail.com>
2021-04-13 21:42:33 +0000

    add save and load checkpoint

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import torch
+import os
 import argparse
 from tqdm import tqdm
 from data.datasets import AnimeFacesDataset
 from torch.utils.data import DataLoader
+from config import cfg
 
 
 def get_mean_std(dataloader):
@@ -17,6 +19,33 @@ def get_mean_std(dataloader):
     std = (channels_squared_sum / num_batches - mean**2)**0.5
 
     return mean, std
+
+
+def checkpoint(epoch, gen, disc, opt_gen, opt_disc):
+    print("=> Saving checkpoint")
+    torch.save({
+        'gen': gen.state_dict(),
+        'disc': disc.state_dict(),
+        'opt_gen': opt_gen.state_dict(),
+        'opt_disc': opt_disc.state_dict(),
+    }, f"{cfg.SAVE_MODELS_PATH}epoch_{epoch}.pth.tar")
+    print(f"=> Checkpoint save to {cfg.SAVE_MODELS_PATH}")
+
+
+def load_checkpoint(checkpoint, gen, disc, opt_gen, opt_disc):
+    print("=> Load checkpoint...")
+    gen.load_state_dict(checkpoint['gen'])
+    disc.load_state_dict(checkpoint['disc'])
+    opt_gen.load_state_dict(checkpoint['opt_gen'])
+    opt_disc.load_state_dict(checkpoint['opt_disc'])
+    print("=> Checkpoint loaded")
+
+
+def load_gen(gen, filename):
+    print("=> Load generator...")
+    gen_path = os.path.join(os.getcwd(), filename)
+    gen.load_state_dict(torch.load(gen_path))
+    print(f"=> Generator model loaded from {gen_path}")
 
 
 def parse_args():
