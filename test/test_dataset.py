4b9432be2b03f4595f7b215d6e2a28f286450097
ErrorInever <errorinever@gmail.com>
2021-04-13 00:05:54 +0000

    fix spaces

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -9,7 +9,7 @@ plt.rcParams["figure.figsize"] = (8, 8)
 plt.gca().set_axis_off()
 plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
                     hspace=0, wspace=0)
-plt.margins(0,0)
+plt.margins(0, 0)
 plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
