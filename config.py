49a6a0671ff32ebab00ed690cd5543e6bb03be8e
ErrorInever <errorinever@gmail.com>
2021-04-21 21:45:52 +0000

    update params

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ cfg = __C
 __C.PROJECT_NAME = "DCGAN-Anime-Faces"
 __C.PROJECT_VERSION_NAME = "Default-DCGAN"
 # Global
-__C.NUM_EPOCHS = 1000
-__C.LEARNING_RATE = 2e-4
-__C.BATCH_SIZE = 64
+__C.NUM_EPOCHS = 50
+__C.LEARNING_RATE = 0.0002
+__C.BATCH_SIZE = 128
 __C.DATASET_SIZE = None
 # CHANNELS
-__C.IMG_SIZE = 128
+__C.IMG_SIZE = 64
 __C.CHANNELS_IMG = 3
 __C.Z_DIMENSION = 128
 # Models
 __C.FEATURES_DISC = 64
 __C.FEATURES_GEN = 64
 # Paths and saves
-__C.SAVE_EACH_EPOCH = 100
+__C.SAVE_EACH_EPOCH = 25
 __C.OUT_DIR = ""
 __C.SAVE_CHECKPOINT_PATH = ""
 
 # Display results
-__C.NUM_SAMPLES = 36    # size grid for display images
+__C.NUM_SAMPLES = 64    # size grid for display images
 __C.FREQ = 50
