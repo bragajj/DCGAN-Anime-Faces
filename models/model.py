f8f2dde230a7fb1e1519b240ebd05f101d107453
ErrorInever <errorinever@gmail.com>
2021-04-21 21:46:30 +0000

    update models

--- models/model.py
+++ models/model.py
@@ -11,15 +11,14 @@ class Generator(nn.Module):
         """
         super().__init__()
         self.body = nn.Sequential(
-            Generator._default_block(channels_noise, features_gen * 16, 4, 1, 0),    # 4x4
-            Generator._default_block(features_gen * 16, features_gen * 8, 4, 2, 1),  # 8x8
-            Generator._default_block(features_gen * 8, features_gen * 4, 4, 2, 1),   # 16x16
-            Generator._default_block(features_gen * 4, features_gen * 2, 4, 2, 1),   # 32x32
-            Generator._default_block(features_gen * 2, features_gen, 4, 2, 1),       # 64x64
+            Generator._default_block(channels_noise, features_gen * 8, 4, 1, 0),    # 4x4
+            Generator._default_block(features_gen * 8, features_gen * 4, 4, 2, 1),  # 8x8
+            Generator._default_block(features_gen * 4, features_gen * 2, 4, 2, 1),   # 16x16
+            Generator._default_block(features_gen * 2, features_gen, 4, 2, 1),   # 32x32
             nn.ConvTranspose2d(
-                features_gen, channels_img, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1)),
+                features_gen, channels_img, kernel_size=4, stride=2, padding=1, bias=False),
             nn.Tanh()
-            # out dimension: [N x 3 x 128 x 128] with range [-1, 1]
+            # out dimension: [N x 3 x 64 x 64] with range [-1, 1]
         )
 
     @staticmethod
@@ -27,11 +26,43 @@ class Generator(nn.Module):
         return nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.2, inplace=True)
+            nn.ReLU(True)
         )
 
     def forward(self, x):
         return self.body(x)
+
+
+"""
+Generator
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+   ConvTranspose2d-1            [-1, 512, 4, 4]       1,048,576
+       BatchNorm2d-2            [-1, 512, 4, 4]           1,024
+              ReLU-3            [-1, 512, 4, 4]               0
+   ConvTranspose2d-4            [-1, 256, 8, 8]       2,097,152
+       BatchNorm2d-5            [-1, 256, 8, 8]             512
+              ReLU-6            [-1, 256, 8, 8]               0
+   ConvTranspose2d-7          [-1, 128, 16, 16]         524,288
+       BatchNorm2d-8          [-1, 128, 16, 16]             256
+              ReLU-9          [-1, 128, 16, 16]               0
+  ConvTranspose2d-10           [-1, 64, 32, 32]         131,072
+      BatchNorm2d-11           [-1, 64, 32, 32]             128
+             ReLU-12           [-1, 64, 32, 32]               0
+  ConvTranspose2d-13            [-1, 3, 64, 64]           3,075
+             Tanh-14            [-1, 3, 64, 64]               0
+================================================================
+Total params: 3,806,083
+Trainable params: 3,806,083
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.00
+Forward/backward pass size (MB): 3.00
+Params size (MB): 14.52
+Estimated Total Size (MB): 17.52
+----------------------------------------------------------------
+"""
 
 
 class Discriminator(nn.Module):
@@ -39,13 +70,12 @@ class Discriminator(nn.Module):
     def __init__(self, in_channels, features_d):
         super().__init__()
         self.body = nn.Sequential(
-            nn.Conv2d(in_channels, features_d, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1)),
-            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels, features_d, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
             Discriminator._default_block(features_d, features_d * 2, 4, 2, 1),
             Discriminator._default_block(features_d * 2, features_d * 4, 4, 2, 1),
             Discriminator._default_block(features_d * 4, features_d * 8, 4, 2, 1),
-            Discriminator._default_block(features_d * 8, features_d * 16, 4, 2, 1),
-            nn.Conv2d(features_d * 16, 1, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0)),
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
             nn.Sigmoid(),
         )
 
@@ -59,6 +89,37 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.body(x)
+
+
+"""
+Discriminator
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+            Conv2d-1           [-1, 64, 32, 32]           3,136
+         LeakyReLU-2           [-1, 64, 32, 32]               0
+            Conv2d-3          [-1, 128, 16, 16]         131,072
+       BatchNorm2d-4          [-1, 128, 16, 16]             256
+         LeakyReLU-5          [-1, 128, 16, 16]               0
+            Conv2d-6            [-1, 256, 8, 8]         524,288
+       BatchNorm2d-7            [-1, 256, 8, 8]             512
+         LeakyReLU-8            [-1, 256, 8, 8]               0
+            Conv2d-9            [-1, 512, 4, 4]       2,097,152
+      BatchNorm2d-10            [-1, 512, 4, 4]           1,024
+        LeakyReLU-11            [-1, 512, 4, 4]               0
+           Conv2d-12              [-1, 1, 1, 1]           8,193
+          Sigmoid-13              [-1, 1, 1, 1]               0
+================================================================
+Total params: 2,765,633
+Trainable params: 2,765,633
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.05
+Forward/backward pass size (MB): 2.31
+Params size (MB): 10.55
+Estimated Total Size (MB): 12.91
+----------------------------------------------------------------
+"""
 
 
 def init_weights(model):
